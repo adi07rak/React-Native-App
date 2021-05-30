0cac9adb866a47b4ac59e111bd3736cd4f4225c8
aditya13jani <44555223+aditya13jani@users.noreply.github.com>
2021-05-29 23:08:50 -0400

    next changes!!!

--- App.js
+++ App.js
@@ -4,9 +4,16 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View >
+    <View style={{padding:50}}>
       <View>
-        <TextInput />
+        <TextInput 
+          placeholder="Course Goal"
+          style={{
+            borderColor:"black",
+            borderWidth:1,
+            padding:10
+          }} 
+        />
       </View>
       <View>
         <Button title="ADD" />
